4ae523227a54792720c5626c54c580b3a44f3818
Alokcseit <swainalokkumar25@gmail.com>
2024-11-07 17:45:52 +0530

    final commit of usestate with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const[name,setName]=useState([])
+  const[currentname,setcurrentname]=useState("")
 
+  const onchangeHandler=(e)=>{
+   setcurrentname(e.target.value)
+  }
+  const Setnewname=()=>{
+     setName(prev=>[...prev,currentname])
+     setcurrentname("")
+  }
+  console.log(name)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <input type='text' value={currentname} placeholder='enter your name' onChange={onchangeHandler} />
+       <button onClick={Setnewname} >Add Name</button>
+       <p>Name are</p>
+       <ul>
+        {
+        name.map(names=>(<li>{names}</li>))
+        }
+       </ul>
     </>
   )
 }
